fe1c4e118c8111768216e29203e094afe918896f
EricSanchezok <niexiaohangeric@163.com>
2025-08-26 15:17:17 +0800

    同步一下

--- service/daily_report.py
+++ service/daily_report.py
@@ -265,5 +265,5 @@ def schedule_daily():
 
 if __name__ == "__main__":
     # _flush_daily_reports("20250820")
-    # run_for_date("20250825")
-    schedule_daily()
+    run_for_date("20250814")
+    # schedule_daily()
