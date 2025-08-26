b6c12ff52d83555416ecbecdd03403223c699af0
EricSanchezok <niexiaohangeric@163.com>
2025-08-26 14:58:23 +0800

    同步

--- agents/PaperFilterAgent/__main__.py
+++ agents/PaperFilterAgent/__main__.py
@@ -1,10 +1,11 @@
 from agents.PaperFilterAgent.agent import PaperFilterAgent
 import asyncio
 
-async def main():
-    agent = PaperFilterAgent()
-    await agent._initialize_qiji_memory()
-    agent.run_server()
+agent = PaperFilterAgent()
 
+async def init():
+    await agent._initialize_qiji_memory()
+    
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(init())
+    agent.run_server()
