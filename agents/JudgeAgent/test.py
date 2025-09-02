50189f7c3752aa536e390185adad870a0a3bbe10
EricSanchezok <niexiaohangeric@163.com>
2025-09-02 17:24:03 +0800

    决定先同步一版

--- agents/JudgeAgent/test.py
+++ agents/JudgeAgent/test.py
@@ -12,9 +12,10 @@ from agents.JudgeAgent.config import ROUTING_AGENT_BASE_URL
 
 if __name__ == "__main__":
     message = {"search_papers": {"task_description": "搜索7月13号AI 领域的论文", "depends_on": [],"agent": "ArxivSearchAgent", "agent_url": ""}}
-
+    message = "你好呀，你今天过得怎么样"
+    base_url = "http://76358938.r8.cpolar.top/http://10.244.9.104:9900/"
     response = asyncio.run(send_message_to_a2a_agent(
-        base_url=ROUTING_AGENT_BASE_URL,
+        base_url=base_url,
         message=json.dumps(message, ensure_ascii=False, indent=4),
         message_id=None,
         role="user",
